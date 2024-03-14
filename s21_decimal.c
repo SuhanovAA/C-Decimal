86303937c5a72d9f421f0f2580a396891ef12008
Alexey <Ortoped149@gmail.com>
2024-03-14 17:22:08 +0700

    update

--- s21_decimal.c
+++ s21_decimal.c
@@ -1,132 +1,70 @@
 #include "s21_decimal.h"
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-  int error = (check_decimal(value_1) && check_decimal(value_2));
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) ;
+// {
+  // int error = (check_decimal(value_1) && check_decimal(value_2));
 
-  if (!error) {
-    big_decimal big_value_1, big_value_2, big_result;
-    int sign_1 = get_sign_decimal(value_1);
-    int sign_2 = get_sign_decimal(value_2);
-    int scale_1 = get_scale_decimal(value_1);
-    int scale_2 = get_scale_decimal(value_2);
+  // if (!error) {
+  //   big_decimal big_value_1, big_value_2, big_result;
+  //   int sign_1 = get_sign_decimal(value_1);
+  //   int sign_2 = get_sign_decimal(value_2);
+  //   int scale_1 = get_scale_decimal(value_1);
+  //   int scale_2 = get_scale_decimal(value_2);
 
-    if (scale_1 > scale_2)
-      decimal_normalization(&value_2, scale_1 - scale_2);
-    else if (scale_1 < scale_2)
-      decimal_normalization(&value_1, scale_2 - scale_1);
+  //   if (scale_1 > scale_2)
+  //     decimal_normalization(&value_2, scale_1 - scale_2);
+  //   else if (scale_1 < scale_2)
+  //     decimal_normalization(&value_1, scale_2 - scale_1);
 
-    convert_decimal_to_big_decimal(value_1, &big_value_1);
-    convert_decimal_to_big_decimal(value_2, &big_value_2);
+  //   convert_decimal_to_big_decimal(value_1, &big_value_1);
+  //   convert_decimal_to_big_decimal(value_2, &big_value_2);
 
-    bitwise_addition(big_value_1, big_value_2, &big_result);
+  //   bitwise_addition(big_value_1, big_value_2, &big_result);
 
-    // if (!check_overflow_big_decimal(big_result)){
-    //     error = 1;
-    // }else{
-    if (sign_1 + sign_2) {
-      invert_sign_big_decimal(&big_result);
-    }
-    convert_big_decimal_to_decimal(big_result, result);
-    // }
-  }
+  //   if (!check_overflow_big_decimal(big_result)){
+  //       error = 1;
+  //   }else{
+  //   if (sign_1 + sign_2) {
+  //     invert_sign_big_decimal(&big_result);
+  //   }
+  //   convert_big_decimal_to_decimal(big_result, result);
+  //   }
+  // }
 
-  return error;
-}
+  // return error;
+// }
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 
 /*  <-----  Comparison Operators    ----->  */
 
-/**
- * Проверка, что value_1 > value_2
- *
- * @param value_1
- * @param value_2
- *
- * @return 0 - FALSE \
- * @return 1 - TRUE
- */
 int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
   return compare(value_1, value_2) == 1 ? 1 : 0;
 }
 
-/**
- * Проверка, что value_1 >= value_2
- *
- * @param value_1
- * @param value_2
- *
- * @return 0 - FALSE \
- * @return 1 - TRUE
- */
 int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return compare(value_1, value_2) >= 0 ? 1 : 0;
 }
 
-/**
- * Проверка, что value_1 < value_2
- *
- * @param value_1
- * @param value_2
- *
- * @return 0 - FALSE \
- * @return 1 - TRUE
- */
 int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
   return compare(value_1, value_2) == -1 ? 1 : 0;
 }
 
-/**
- * Проверка, что value_1 <= value_2
- *
- * @param value_1
- * @param value_2
- *
- * @return 0 - FALSE \
- * @return 1 - TRUE
- */
 int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return compare(value_1, value_2) <= 0 ? 1 : 0;
 }
 
-/**
- * Проверка, что value_1 == value_2
- *
- * @param value_1
- * @param value_2
- *
- * @return 0 - FALSE \
- * @return 1 - TRUE
- */
 int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
   return compare(value_1, value_2) ? 0 : 1;
 }
 
-/**
- * Проверка, что value_1 != value_2
- *
- * @param value_1
- * @param value_2
- *
- * @return 0 - FALSE \
- * @return 1 - TRUE
- */
 int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2) {
   return compare(value_1, value_2) ? 1 : 0;
 }
 
 /*  <-----  Transform Functions    ----->  */
 
-/**
- * Преобразование из float в int.
- *
- * @param src число float (откуда переводится)
- * @param *dst число decimal (куда переводится)
- *
- * @return 0 - ОК \
- * @return 1 - ОШИБКА КОНВЕРТАЦИИ
- */
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
   int error = 0;
   nullify_decimal(dst);
@@ -169,15 +107,7 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
 
   return error;
 }
-/**
- * Преобразование из decimal в float.
- *
- * @param src число float (откуда переводится)
- * @param *dst число decimal (куда переводится)
- *
- * @return 0 - ОК \
- * @return 1 - ОШИБКА КОНВЕРТАЦИИ
- */
+
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
   int error = check_decimal(src);
   if (!error) {
@@ -200,15 +130,6 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
   return error;
 }
 
-/**
- * Преобразование из int в decimal.
- *
- * @param src число float (откуда переводится)
- * @param *dst число decimal (куда переводится)
- *
- * @return 0 - ОК \
- * @return 1 - ОШИБКА КОНВЕРТАЦИИ
- */
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
   nullify_decimal(dst);
   dst->bits[0] = abs(src);
@@ -216,15 +137,6 @@ int s21_from_int_to_decimal(int src, s21_decimal *dst) {
   return 0;
 }
 
-/**
- * Преобразование из decimal в int.
- *
- * @param src число float (откуда переводится)
- * @param *dst число decimal (куда переводится)
- *
- * @return 0 - ОК \
- * @return 1 - ОШИБКА КОНВЕРТАЦИИ
- */
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
   int error = 0;
   double mantissa = 0;
@@ -252,30 +164,8 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst) {
 
 int s21_floor(s21_decimal value, s21_decimal *result);
 int s21_round(s21_decimal value, s21_decimal *result);
+int s21_truncate(s21_decimal value, s21_decimal *result);
 
-/**
- * Возвращает целые цифры указанного Decimal числа; любые дробные цифры
- * отбрасываются, включая конечные нули.
- *
- * @param value входящий массив
- * @param result исходящий (результат операции) массив
- *
- * @return 0 - OK \
- * @return 1 - ошибка вычисления
- */
-int s21_truncate(s21_decimal value, s21_decimal *result) ;
-
-/**
- * Возвращает результат умножения указанного Decimal на -1.
- * Массив value проверяется на overflov. Если проверка пройдена, то копируется
- * value в result и меняется знак на противоположный в result
- *
- * @param value входящий массив
- * @param result исходящий (результат операции) массив
- *
- * @return 0 - OK \
- * @return 1 - ошибка вычисления
- */
 int s21_negate(s21_decimal value, s21_decimal *result) {
   int error = check_decimal(value);
   if (!error) {
@@ -285,23 +175,10 @@ int s21_negate(s21_decimal value, s21_decimal *result) {
   return error;
 }
 
-/**
- * Сравнение value_1 и value_2. Если порядок не равен 0, то проводится
- * нормализация порядка определенного массива. После чего вызывается функция
- * is_greater, которая проверяет разницу между битами.
- *
- * @param value_1
- * @param value_2
- *
- * @return - -1 - при value_1 < value_2
- * @return - 0 - при value_1 == value_2
- * @return - 1 - при value_1 > value_2
- */
 int compare(s21_decimal value_1, s21_decimal value_2) {
   int result_compare = 0;
   int sign_1 = get_sign_decimal(value_1);
   int sign_2 = get_sign_decimal(value_2);
-
   if (!check_decimal_zero(value_1) || !check_decimal_zero(value_2)) {
     if (!sign_1 && sign_2) {
       result_compare = 1;
@@ -363,58 +240,10 @@ int is_greater(s21_decimal value_1, s21_decimal value_2) {
   return result;
 }
 
-#include "s21_decimal.h"
-
 void copy_decimal(s21_decimal *new__decimal_array, s21_decimal src) {
   for (int i = 0; i < SIZE_DECIMAL; i++) {
     new__decimal_array->bits[i] = src.bits[i];
   }
-}
-
-void copy_big_decimal(s21_decimal *new_big_decimal_array, s21_decimal src) {
-  for (int i = 0; i < SIZE_BIG_DECIMAL; i++) {
-    new_big_decimal_array->bits[i] = src.bits[i];
-  }
-}
-
-int big_decimal_check_equal_bits(big_decimal value_1, big_decimal value_2) {
-  int result = 0;
-  for (int i = 223; i >= 0 && !result; i--) {
-    result = get_bit_big_decimal(value_1, i) - get_bit_big_decimal(value_2, i);
-  }
-  return result;
-}
-
-int decimal_bits_is_equal_zero(s21_decimal value) {
-  int result = 1;
-  for (int i = 0; i < 96 && result == 1; i++) {
-    if (!get_bit_decimal(value, i)) {
-      result = 0;
-    }
-  }
-  return result;
-}
-
-int big_decimal_bits_is_equal_zero(big_decimal value) {
-  int result = 1;
-  for (int i = 0; i < 224 && result == 1; i++) {
-    if (!get_bit_big_decimal(value, i)) {
-      result = 0;
-    }
-  }
-  return result;
-}
-
-void big_decimal_normalization(big_decimal *dst, int diff) {
-  int scale = get_scale_big_decimal(*dst);
-  for (int i = 0; i < diff; i++) {
-    big_decimal temp_1 = *dst;
-    big_decimal temp_2 = *dst;
-    big_decimal_shift_left(&temp_1, 1);
-    big_decimal_shift_left(&temp_2, 3);
-    big_decimal_summ(temp_1, temp_2, dst);
-  }
-  set_scale_big_decimal(dst, scale + diff);
 }
 
 void decimal_normalization(s21_decimal *dst, int diff) {
@@ -429,18 +258,6 @@ void decimal_normalization(s21_decimal *dst, int diff) {
   set_scale_decimal(dst, scale + diff);
 }
 
-void big_decimal_shift_left(big_decimal *value, int shift) {
-  if (shift) {
-    unsigned memory = 0;
-    for (int i = 0; i < 7; ++i) {
-      unsigned temp = value->bits[i];
-      value->bits[i] <<= shift;
-      value->bits[i] |= memory;
-      memory = temp >> (32 - shift);
-    }
-  }
-}
-
 void decimal_shift_left(s21_decimal *value, int shift) {
   if (shift) {
     unsigned memory = 0;
@@ -450,19 +267,6 @@ void decimal_shift_left(s21_decimal *value, int shift) {
       value->bits[i] |= memory;
       memory = temp >> (32 - shift);
     }
-  }
-}
-
-void big_decimal_summ(big_decimal value_1, big_decimal value_2,
-                      big_decimal *result) {
-  int memory = 0;
-  unsigned tmp = 0;
-  for (int i = 0; i < 224; i++) {
-    tmp = get_bit_big_decimal(value_1, i) + get_bit_big_decimal(value_2, i) +
-          memory;
-    memory = tmp / 2;
-    tmp %= 2;
-    set_bit_big_decimal(result, i, tmp);
   }
 }
 
@@ -478,43 +282,9 @@ void decimal_summ(s21_decimal value_1, s21_decimal value_2,
   }
 }
 
-void big_decimal_diff(big_decimal value_1, big_decimal value_2,
-                      big_decimal *result) {
-  int memory = 0;
-  int tmp = 0;
-  for (int i = 0; i < 224; i++) {
-    tmp = get_bit_big_decimal(value_1, i) - get_bit_big_decimal(value_2, i) -
-          memory;
-    memory = tmp < 0 ? 1 : 0;
-    tmp %= 2;
-    set_bit_big_decimal(result, i, tmp);
-  }
-}
-
 void nullify_decimal(s21_decimal *dst) {
   int i;
   for (i = 0; i < SIZE_DECIMAL; i++) {
-    dst->bits[i] = 0;
-  }
-}
-
-void nullify_big_decimal(big_decimal *dst) {
-  int i;
-  for (i = 0; i < SIZE_BIG_DECIMAL; i++) {
-    dst->bits[i] = 0;
-  }
-}
-
-void nullify_mantissa_decimal(s21_decimal *dst) {
-  int i;
-  for (i = 0; i <= SIZE_DECIMAL_MANTISSA; i++) {
-    dst->bits[i] = 0;
-  }
-}
-
-void nullify_mantissa_big_decimal(big_decimal *dst) {
-  int i;
-  for (i = 0; i <= SIZE_BIG_DECIMAL_MANTISSA; i++) {
     dst->bits[i] = 0;
   }
 }
@@ -523,17 +293,6 @@ int check_decimal_zero(s21_decimal value) {
   int i;
   int check_zero = 1;
   for (i = 0; i <= SIZE_DECIMAL_MANTISSA && check_zero != 0; i++) {
-    if (value.bits[i] != 0) {
-      check_zero = 0;
-    }
-  }
-  return check_zero;
-}
-
-int check_big_decimal_zero(big_decimal value) {
-  int i;
-  int check_zero = 1;
-  for (i = 0; i <= SIZE_BIG_DECIMAL_MANTISSA && check_zero != 0; i++) {
     if (value.bits[i] != 0) {
       check_zero = 0;
     }
@@ -555,43 +314,9 @@ int get_bit_decimal(s21_decimal value, int bit_index) {
   return (int)result ? 1 : 0;
 }
 
-int get_bit_big_decimal(big_decimal value, int bit_index) {
-  unsigned int result = 0, bits_index = 0, flag = 0;
-  const unsigned int step = 32;
-  int i = 32;
-  for (i = step; bits_index <= SIZE_BIG_DECIMAL && flag != 1;
-       i += step, bits_index++) {
-    if (i > bit_index) {
-      result = value.bits[bits_index] & 1 << bit_index;
-      flag = 1;
-    }
-  }
-  return (int)result ? 1 : 0;
-}
-
-void set_bit_big_decimal(big_decimal *dst, int bit_index, int bit_value) {
-  unsigned int bits_index = 0, flag = 0;
-  const unsigned int step = 32;
-  int i = 32;
-  for (i = step; bits_index <= SIZE_BIG_DECIMAL && flag != 1;
-       i += step, bits_index++) {
-    if (i > bit_index) {
-      dst->bits[bits_index] = (dst->bits[bits_index] & ~(1 << bit_index)) |
-                              (bit_value << bit_index);
-      flag = 1;
-    }
-  }
-}
-
 int get_sign_decimal(s21_decimal value) {
   int value_sign;
   value_sign = get_bit_decimal(value, 127);
-  return value_sign;
-}
-
-int get_sign_big_decimal(big_decimal value) {
-  int value_sign;
-  value_sign = get_bit_big_decimal(value, 255);
   return value_sign;
 }
 
@@ -599,117 +324,60 @@ int get_scale_decimal(s21_decimal value) {
   return (int)((unsigned int)(value.bits[3] & ~(1 << 31)) >> 16);
 }
 
-int get_scale_big_decimal(big_decimal value) {
-  return (int)((unsigned int)(value.bits[7] & ~(1 << 31)) >> 16);
-}
-
 void set_scale_decimal(s21_decimal *value, int scale) {
   value->bits[3] &= ~(127 << 16);
   value->bits[3] |= scale << 16;
-}
-
-s21_decimal *s21_set_scale(s21_decimal *decimal, int scale) {
-  int sign = 0;
-  sign = get_sign_decimal(*decimal);
-  if (scale >= 0 && scale <= 28) {
-    decimal->bits[3] &= 0;
-    decimal->bits[3] |= (scale <<= 16);
-    set_bit_decimal(decimal, 127, sign);
-  }
-  return decimal;
-}
-
-void set_scale_big_decimal(big_decimal *value, int scale) {
-  value->bits[7] &= ~(255 << 16);
-  value->bits[7] |= scale << 16;
 }
 
 void invert_sign_decimal(s21_decimal *dst) {
   set_bit_decimal(dst, 127, ((get_sign_decimal(*dst) + 1) % 2));
 }
 
-void invert_sign_big_decimal(big_decimal *dst) {
-  set_bit_big_decimal(dst, 255, ((get_sign_big_decimal(*dst) + 1) % 2));
-}
+// void convert_decimal_to_big_decimal(s21_decimal value, big_decimal *dst) {
+//   nullify_big_decimal(dst);
+//   int i, j;
+//   for (i = 0; i <= SIZE_DECIMAL_MANTISSA; i++) {
+//     for (j = 31; j >= 0; j--) {
+//       dst->bits[i] |= value.bits[i] & (1 << j);
+//     }
+//   }
+//   for (j = 31; j >= 0; j--) {
+//     dst->bits[SIZE_BIG_DECIMAL - 1] |= value.bits[SIZE_DECIMAL - 1] & (1 << j);
+//   }
+// }
 
-void convert_decimal_to_big_decimal(s21_decimal value, big_decimal *dst) {
-  nullify_big_decimal(dst);
-  int i, j;
-  for (i = 0; i <= SIZE_DECIMAL_MANTISSA; i++) {
-    for (j = 31; j >= 0; j--) {
-      dst->bits[i] |= value.bits[i] & (1 << j);
-    }
-  }
-  for (j = 31; j >= 0; j--) {
-    dst->bits[SIZE_BIG_DECIMAL - 1] |= value.bits[SIZE_DECIMAL - 1] & (1 << j);
-  }
-}
+// int convert_big_decimal_to_decimal(big_decimal value, s21_decimal *dst) {
+//   int error_convert = OK;
+//   nullify_decimal(dst);
+//   if (!check_overflow_big_decimal(value) || get_scale_big_decimal(value) > 28) {
+//     error_convert = ERROR_OVERFLOW;
+//   } else {
+//     for (int i = 0; i < 3; ++i) {
+//       for (int j = 31; j >= 0; --j) {
+//         dst->bits[i] = dst->bits[i] | (value.bits[i] & (1 << j));
+//       }
+//     }
+//     for (int j = 31; j >= 0; --j) {
+//       dst->bits[3] = dst->bits[3] | (value.bits[7] & (1 << j));
+//     }
+//   }
+//   return error_convert;
+// }
 
-int convert_big_decimal_to_decimal(big_decimal value, s21_decimal *dst) {
-  int error_convert = OK;
-  nullify_decimal(dst);
-  if (!check_overflow_big_decimal(value) || get_scale_big_decimal(value) > 28) {
-    error_convert = ERROR_OVERFLOW;
-  } else {
-    for (int i = 0; i < 3; ++i) {
-      for (int j = 31; j >= 0; --j) {
-        dst->bits[i] = dst->bits[i] | (value.bits[i] & (1 << j));
-      }
-    }
-    for (int j = 31; j >= 0; --j) {
-      dst->bits[3] = dst->bits[3] | (value.bits[7] & (1 << j));
-    }
-  }
-  return error_convert;
-}
-
-int s21_get_exponent(big_decimal dec) {
-  return (int)((unsigned int)(dec.bits[7] & ~(1 << 31)) >> 16);
-}
-
-void s21_set_exponent(big_decimal *dec, int value) {
-  dec->bits[7] &= ~(255 << 16);
-  dec->bits[7] |= value << 16;
-}
-
-void bitwise_addition(big_decimal value_1, big_decimal value_2,
-                      big_decimal *result) {
-  unsigned memo = 0;
-  for (int i = 0; i < 32 * 7 - 1; ++i) {
-    unsigned result_bit = get_bit_big_decimal(value_1, i) +
-                          get_bit_big_decimal(value_2, i) + memo;
-    memo = result_bit / 2;
-    result_bit %= 2;
-    set_bit_big_decimal(result, i, result_bit);
-  }
-}
-
-// errors
-
-int check_overflow_big_decimal(big_decimal value) {
-  int i;
-  int check_overflow = 1;
-  for (i = 3; i <= SIZE_BIG_DECIMAL_MANTISSA && check_overflow != 0; i++) {
-    if (value.bits[i] != 0) {
-      check_overflow = 0;
-    }
-  }
-  return check_overflow;
-}
+// void bitwise_addition(big_decimal value_1, big_decimal value_2,
+//                       big_decimal *result) {
+//   unsigned memo = 0;
+//   for (int i = 0; i < 32 * 7 - 1; ++i) {
+//     unsigned result_bit = get_bit_big_decimal(value_1, i) +
+//                           get_bit_big_decimal(value_2, i) + memo;
+//     memo = result_bit / 2;
+//     result_bit %= 2;
+//     set_bit_big_decimal(result, i, result_bit);
+//   }
+// }
 
 int check_decimal(s21_decimal value) {
   int error = OK;
   if ((value.bits[3] &= SCALE_ERROR_MASK) != 0) error = ERROR_OVERFLOW;
   return error;
-}
-
-// деление на 10 децимала
-int s21_divide_by_10(s21_decimal *decimal) {
-  int remainder = 0;
-  for (int i = 2; i >= 0; i--) {
-    long int_rewriting = (remainder * 0x100000000) + (long)decimal->bits[i];
-    decimal->bits[i] = int_rewriting / 10;
-    remainder = int_rewriting - (10 * decimal->bits[i]);
-  }
-  return remainder;
 }
--- s21_decimal.h
+++ s21_decimal.h
@@ -17,12 +17,8 @@
 #define SIZE_DECIMAL_MANTISSA 2
 #define SIZE_BIG_DECIMAL_MANTISSA 6
 
-#define SCALE_BITS 0xff0000  // 111111110000000000000000
+// #define SCALE_BITS 0xff0000  // 111111110000000000000000
 #define SCALE_ERROR_MASK 0b01111111100000000111111111111111
-// #define SCALE_MIN_DECIMAL 96 + 16
-// #define SCALE_MIN_BIG_DECIMAL 224 + 16
-// #define SCALE_MAX_DECIMAL 96 + 23
-// #define SCALE_MAX_BIG_DECIMAL 224 + 23
 
 #define OK 0
 #define ERROR 1
@@ -73,209 +69,64 @@ int s21_is_equal(s21_decimal value_1, s21_decimal value_2);
 int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2);
 
 // --- additional_func --- //
+
 /**
- * Функция проходит по каждому индексу массива (4 байт / индексы от 0 до 3
- * включительно) src и копирует индексы в новый массив new_decimal_array.
+ * Сравнение value_1 и value_2. Если порядок не равен 0, то проводится
+ * нормализация порядка определенного массива. После чего вызывается функция
+ * is_greater, которая проверяет разницу между битами.
  *
- * @param new_decimal_array куда копируется
- * @param src откуда копируется
+ * @param value_1
+ * @param value_2
+ *
+ * @return - -1 - при value_1 < value_2
+ * @return - 0 - при value_1 == value_2
+ * @return - 1 - при value_1 > value_2
  */
+int compare(s21_decimal value_1, s21_decimal value_2);
+
+int float_get_bit(double number, int index);
+
+void set_bit_decimal(s21_decimal *dst, int bit_index, int bit_value);
+
+/**
+ * Вычисление разницы между value_1 и value_2 по битам от старшего (95) до
+ * младшего (0).
+ *
+ * @param value_1 децимал число (4 байта)
+ * @param value_2 децимал число (4 байта)
+ *
+ * @return - 0 - Все биты равны
+ * @return - Разница между битами
+ */
+int is_greater(s21_decimal value_1, s21_decimal value_2);
+
 void copy_decimal(s21_decimal *new__decimal_array, s21_decimal src);
 
-/**
- * Функция проходит по каждому индексу массива (8 байт / индексы от 0 до 7
- * включительно) src и копирует индексы в новый массив new_big_decimal_array.
- *
- * @param new_big_decimal_array куда копируется
- * @param src откуда копируется
- */
-void copy_big_decimal(s21_decimal *new_big_decimal_array, s21_decimal src);
-
-/**
- * Сравнивает два больших децимал числа (массив value_1 и массив value_2).
- * Проходит через каждый бит этих чисел, начиная с самого старшего бита (223 до
- * 0 включительно) и вычисляет разницу между битами двух массивов на каждой
- * позиции.
- *
- * @return (различия не найдены 0 / различие )
- */
-int big_decimal_check_equal_bits(big_decimal value_1, big_decimal value_2);
-
-/**
- * Проверка на ноль децимал числа по индексу с 0 до 95 (вкл) бита ( 3 байта из
- * 4).
- *
- * @param value массив
- * @return 0 - ноль найден / 1 - нули не найдены
- */
-int decimal_bits_is_equal_zero(s21_decimal value);
-
-/**
- * Проверка на ноль большого децимал числа по индексу с 0 до 223 (вкл) бита ( 3
- * байта из 4).
- *
- * @param value массив
- * @return 0 - если найден ноль и 1 - если не найден 0
- */
-int big_decimal_bits_is_equal_zero(big_decimal value);
-
-void big_decimal_normalization(big_decimal *dst, int diff);
-void big_decimal_shift_left(big_decimal *value, int shift);
-
-void big_decimal_summ(big_decimal value_1, big_decimal value_2,
-                      big_decimal *result);
-void big_decimal_diff(big_decimal value_1, big_decimal value_2,
-                      big_decimal *result);
-
 void decimal_normalization(s21_decimal *dst, int diff);
+
 void decimal_shift_left(s21_decimal *value, int shift);
 
 void decimal_summ(s21_decimal value_1, s21_decimal value_2,
                   s21_decimal *result);
 
-/**
- * Зануление массивов->структур s21_decimal и big_decimal
- *
- * @param dst Адрес на структуры
- */
 void nullify_decimal(s21_decimal *dst);
-void nullify_big_decimal(big_decimal *dst);
 
-/**
- * Зануление мантиссы массивов->структур s21_decimal и big_decimal
- *
- * @param dst Адрес на структуры
- */
-void nullify_mantissa_decimal(s21_decimal *dst);
-void nullify_mantissa_big_decimal(big_decimal *dst);
-
-/**
- * Проверка на 0 массивов->структур s21_decimal и big_decimal
- *
- * @param value значение в структуре
- * @return (1 - нулевая / 0 - не нулевая)
- */
 int check_decimal_zero(s21_decimal value);
-int check_big_decimal_zero(big_decimal value);
 
-/**
- * (get) bit для структур
- *
- * @param value просмотреть значение из массива
- * @param bit_index индекс где смотреть бит
- * @return 1:0 - найденный бит
- */
 int get_bit_decimal(s21_decimal value, int bit_index);
-int get_bit_big_decimal(big_decimal value, int bit_index);
 
-/**
- * (set) bit для структур
- *
- * @param dst адрес массива
- * @param bit_index индекс куда ставить бит
- * @param bit_value значение бита
- */
-void set_bit_decimal(s21_decimal *dst, int bit_index, int bit_value);
-void set_bit_big_decimal(big_decimal *dst, int bit_index, int bit_value);
-
-/**
- * (get) Считать знак
- *
- * @param value просмотреть значение (знака) из массива
- * @return 1:0 - найденный бит (знак)
- */
 int get_sign_decimal(s21_decimal value);
-int get_sign_big_decimal(big_decimal value);
 
-/**
- * Взятие экспоненты из decimal числа. Проход с 23 по 16 бит 4 байта.
- *
- * @param value decimal число
- * @return значение экспоненты (степени) decimal числа
- */
 int get_scale_decimal(s21_decimal value);
 
-/**
- * Взятие экспоненты из big_decimal числа. Проход с 23 по 16 бит 8 байта.
- *
- * @param value big_decimal число
- * @return значение экспоненты (степени) big_decimal числа
- */
-int get_scale_big_decimal(big_decimal value);
-
-/**
- * Установка экспоненты decimal числа (4 байт).
- *
- * @param value массив decimal числа.
- * @param scale новая экспонента.
- */
 void set_scale_decimal(s21_decimal *value, int scale);
 
-/**
- * Установка экспоненты big_decimal числа (8 байт).
- *
- * @param value массив big_decimal числа.
- * @param scale новая экспонента.
- */
-void set_scale_big_decimal(big_decimal *value, int scale);
-
-/**
- * Поменять знак (позиция 127 бита) децимал числа на противоположный.
- *
- * @param dst адрес массива
- */
 void invert_sign_decimal(s21_decimal *dst);
 
-/**
- * Поменять знак (позиция 255 бита) биг децимал числа на противоположный.
- *
- * @param dst адрес массива
- */
-void invert_sign_big_decimal(big_decimal *dst);
+// void convert_decimal_to_big_decimal(s21_decimal value, big_decimal *dst);
 
-/**
- * Конвертация из decimal в big_decimal. Проверки 4 байта decimal нет.
- *
- * @param value значение массива
- * @param dst куда конвертировать
- */
-void convert_decimal_to_big_decimal(s21_decimal value, big_decimal *dst);
+// int convert_big_decimal_to_decimal(big_decimal value, s21_decimal *dst);
 
-/**
- * Конвертация из big_decimal в decimal. Есть проверка на overflow big_decimal
- * числа.
- *
- * @param value значение массива
- * @param dst куда конвертировать
- */
-int convert_big_decimal_to_decimal(big_decimal value, s21_decimal *dst);
-
-/**
- * Проверка на overflow big_decomal
- *
- * @param value значение в структуре big_decimal
- * @return (1 - ОК / 0 - overflow)
- */
-int check_overflow_big_decimal(big_decimal value);
-
-/**
- * Проверка последнего байта.
- * - Биты от 0 до 15, младшее слово, не используются и должны быть равны нулю.
- * - Биты с 24 по 30 не используются и должны быть равны нулю.
- *
- * @param value значение массива
- * @return (0 - OK / 1 - OVERFLOW)
- */
 int check_decimal(s21_decimal value);
 
-void bitwise_addition(big_decimal value_1, big_decimal value_2,
-                      big_decimal *result);
-
-
-int is_greater(s21_decimal value_1, s21_decimal value_2);
-int compare(s21_decimal value_1, s21_decimal value_2);
-int s21_divide_by_10(s21_decimal *decimal);
-s21_decimal *s21_set_scale(s21_decimal *decimal, int scale);
-long double s21_fmod(double x, double y);
-int float_get_bit(double number, int index);
 #endif  // S21_DECIMAL_H
